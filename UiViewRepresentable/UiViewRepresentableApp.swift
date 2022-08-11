21f644c166f2834f1a5b57f56e1def642184d18c
Christian Skorobogatow <christiansko@icloud.com>
2022-08-11 11:09:09 +0200

    Initial Commit

---
+++ UiViewRepresentable/UiViewRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  UiViewRepresentableApp.swift
+//  UiViewRepresentable
+//
+//  Created by Christian Skorobogatow on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct UiViewRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
